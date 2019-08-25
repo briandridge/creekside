954cea3bbd5f20fd0351305332ba4216cdab0c32
briandridge <briandridge@gmail.com>
2019-08-25 13:42:22 -0600

    initial commit

---
+++ content/pages/contact.md
@@ -0,0 +1,27 @@
+---
+template: ContactPage
+slug: contact
+title: Get in touch
+featuredImage: https://ucarecdn.com/e22a858a-b420-47af-99f6-ed54b6860333/
+# subtitle: Get in touch with us.
+address: '9801 S Parker Rd, Parker, CO 80134'
+phone: 720 851 8600 
+email: email@creeksidebikes.com
+locations:
+  - lat: '-27.9654732'
+    lng: '153.2432449'
+    mapLink: ''
+meta:
+  description: This is a meta description.
+  title: Contact Page
+---
+
+# Example contact form
+
+This form is setup to use Netlify's form handling:
+
+- the form action is set to the current absolute url: `action: '/contact/'`
+- a name attribute is sent with the form's data `'form-name': 'Contact'`
+- netlify data attributes are added to the form `data-netlify data-netlify-honeypot`
+
+Find out more in the [Netlify Docs](https://www.netlify.com/docs/form-handling/).
---
+++ public/admin/cms.dc4275265f0f9e9964c9.hot-update.js
@@ -0,0 +1,209 @@
+webpackHotUpdate("cms",{
+
+/***/ "./src/templates/ContactPage.js":
+/*!**************************************!*\
+  !*** ./src/templates/ContactPage.js ***!
+  \**************************************/
+/*! exports provided: ContactPageTemplate, default, pageQuery */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* WEBPACK VAR INJECTION */(function(module) {/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "ContactPageTemplate", function() { return ContactPageTemplate; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "pageQuery", function() { return pageQuery; });
+/* harmony import */ var core_js_modules_es6_object_assign__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! core-js/modules/es6.object.assign */ "./node_modules/core-js/modules/es6.object.assign.js");
+/* harmony import */ var core_js_modules_es6_object_assign__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(core_js_modules_es6_object_assign__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var react_feather__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! react-feather */ "./node_modules/react-feather/dist/index.js");
+/* harmony import */ var _components_PageHeader__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../components/PageHeader */ "./src/components/PageHeader.js");
+/* harmony import */ var _components_FormSimpleAjax__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../components/FormSimpleAjax */ "./src/components/FormSimpleAjax.js");
+/* harmony import */ var _components_Content__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ../components/Content */ "./src/components/Content.js");
+/* harmony import */ var _components_GoogleMap__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ../components/GoogleMap */ "./src/components/GoogleMap.js");
+/* harmony import */ var _components_Layout__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ../components/Layout */ "./src/components/Layout.js");
+/* harmony import */ var _ContactPage_css__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! ./ContactPage.css */ "./src/templates/ContactPage.css");
+/* harmony import */ var _ContactPage_css__WEBPACK_IMPORTED_MODULE_8___default = /*#__PURE__*/__webpack_require__.n(_ContactPage_css__WEBPACK_IMPORTED_MODULE_8__);
+
+var _jsxFileName = "/Users/briandridge/myprojects/creekside/src/templates/ContactPage.js";
+
+(function () {
+  var enterModule = (typeof reactHotLoaderGlobal !== 'undefined' ? reactHotLoaderGlobal : __webpack_require__(/*! react-hot-loader */ "./node_modules/react-hot-loader/index.js")).enterModule;
+  enterModule && enterModule(module);
+})();
+
+var __signature__ = typeof reactHotLoaderGlobal !== 'undefined' ? reactHotLoaderGlobal.default.signature : function (a) {
+  return a;
+};
+
+
+
+
+
+
+
+
+ // Export Template for use in CMS preview
+
+var ContactPageTemplate = function ContactPageTemplate(_ref) {
+  var body = _ref.body,
+      title = _ref.title,
+      subtitle = _ref.subtitle,
+      featuredImage = _ref.featuredImage,
+      address = _ref.address,
+      phone = _ref.phone,
+      email = _ref.email,
+      locations = _ref.locations;
+  return react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("main", {
+    className: "Contact",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 23
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_components_PageHeader__WEBPACK_IMPORTED_MODULE_3__["default"], {
+    title: title,
+    subtitle: subtitle,
+    backgroundImage: featuredImage,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 24
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("section", {
+    className: "section Contact--Section1",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 29
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("div", {
+    className: "container Contact--Section1--Container",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 30
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 31
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_components_Content__WEBPACK_IMPORTED_MODULE_5__["default"], {
+    source: body,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 32
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("div", {
+    className: "Contact--Details",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 33
+    },
+    __self: this
+  }, address && react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("a", {
+    className: "Contact--Details--Item",
+    href: "https://www.google.com.au/maps/search/" + encodeURI(address),
+    target: "_blank",
+    rel: "noopener noreferrer",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 35
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(react_feather__WEBPACK_IMPORTED_MODULE_2__["MapPin"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 43
+    },
+    __self: this
+  }), " ", address), phone && react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("a", {
+    className: "Contact--Details--Item",
+    href: "tel:" + phone,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 47
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(react_feather__WEBPACK_IMPORTED_MODULE_2__["Smartphone"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 48
+    },
+    __self: this
+  }), " ", phone), email && react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("a", {
+    className: "Contact--Details--Item",
+    href: "mailto:" + email,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 52
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(react_feather__WEBPACK_IMPORTED_MODULE_2__["Mail"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 53
+    },
+    __self: this
+  }), " ", email))))), react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_components_GoogleMap__WEBPACK_IMPORTED_MODULE_6__["default"], {
+    locations: locations,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 65
+    },
+    __self: this
+  }));
+};
+
+var ContactPage = function ContactPage(_ref2) {
+  var page = _ref2.data.page;
+  return react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(_components_Layout__WEBPACK_IMPORTED_MODULE_7__["default"], {
+    meta: page.frontmatter.meta || false,
+    title: page.frontmatter.title || false,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 70
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement(ContactPageTemplate, Object.assign({}, page.frontmatter, {
+    body: page.html,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 74
+    },
+    __self: this
+  })));
+};
+
+var _default = ContactPage;
+/* harmony default export */ __webpack_exports__["default"] = (_default);
+var pageQuery = "3855609909";
+;
+
+(function () {
+  var reactHotLoader = (typeof reactHotLoaderGlobal !== 'undefined' ? reactHotLoaderGlobal : __webpack_require__(/*! react-hot-loader */ "./node_modules/react-hot-loader/index.js")).default;
+
+  if (!reactHotLoader) {
+    return;
+  }
+
+  reactHotLoader.register(ContactPageTemplate, "ContactPageTemplate", "/Users/briandridge/myprojects/creekside/src/templates/ContactPage.js");
+  reactHotLoader.register(ContactPage, "ContactPage", "/Users/briandridge/myprojects/creekside/src/templates/ContactPage.js");
+  reactHotLoader.register(pageQuery, "pageQuery", "/Users/briandridge/myprojects/creekside/src/templates/ContactPage.js");
+  reactHotLoader.register(_default, "default", "/Users/briandridge/myprojects/creekside/src/templates/ContactPage.js");
+})();
+
+;
+
+(function () {
+  var leaveModule = (typeof reactHotLoaderGlobal !== 'undefined' ? reactHotLoaderGlobal : __webpack_require__(/*! react-hot-loader */ "./node_modules/react-hot-loader/index.js")).leaveModule;
+  leaveModule && leaveModule(module);
+})();
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./../../node_modules/gatsby-plugin-netlify-cms/node_modules/webpack/buildin/harmony-module.js */ "./node_modules/gatsby-plugin-netlify-cms/node_modules/webpack/buildin/harmony-module.js")(module)))
+
+/***/ })
+
+})
+//# sourceMappingURL=cms.dc4275265f0f9e9964c9.hot-update.js.map
